bd98958eff46762e3289aa62604bd41c1356c0b5
Lazizbek <lazizbektojiboyev0611@gmail.com>
2022-02-25 10:47:03 +0500

    Navbar complated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes} from 'react-router-dom'
+import './App.scss'
 
-function App() {
+import { About, Footer, Header, Skills, Testimonials, Work } from './container'
+import { Navbar } from './components'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route path='/skills' component={Skills}/>
+      </Routes>
+      <Header />
+      <About />
+      <Skills />
+      <Testimonials />
+      <Work />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { BiMenuAltRight } from 'react-icons/bi'
+import { VscCloseAll } from 'react-icons/vsc'
+import { Link } from 'react-router-dom'
+import './Navbar.scss'
+
+const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false);
+
+    const toggler = () => {
+        setToggle(!toggle ? true : !toggle)
+        console.log(toggle)
+    }
+
+    return (
+        <nav className='app__navbar'>
+            <div className='app__navbar-logo'>
+                Laziz
+            </div>
+            <ul className='app__navbar-links'>
+                {['home', 'about', 'skills', 'work', 'contact'].map((item, index) => (
+                    <li key={index} className='app__flex p-text'>
+                        <div />
+                        <Link to={`${item}`}>{item}</Link>
+                    </li>
+                ))}
+            </ul>
+
+            <div className="app__navbar-menu" onClick={toggler}>
+                {!toggle ? <BiMenuAltRight /> : <VscCloseAll style={{zIndex: 90}}/>}
+                <ul className={`app__navbar-menu-links ${toggle ? 'block' : 'none'}`}>
+                    {['home', 'about', 'skills', 'work', 'contact'].map((item, index) => (
+                        <li key={index} className='app__flex p-text'>
+                            <div />
+                            <Link to={`${item}`}>{item}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
